c88c46b6a4045bc018e3e62f9702de0b01f63c42
Diwi-J <2589474@students.wits.ac.za>
2025-08-16 17:15:33 +0200

    Main and Pause Menu Scripts and Prefabs (#4)
    * Update Settings.json
    
    * Update Settings.json
    
    * Add main and pause menu UI with scripts
    
    • Created MainMenu and PauseMenu scripts
    • Created main menu and pause menu prefabs

---
+++ Monsieur Flambé’s Bakery/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public bool IsPaused = false;
+
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
